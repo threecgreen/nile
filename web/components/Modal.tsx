6e7e9739d39d036999951640257f4ebbb08e2f8b
Carter Green <cartergr@usc.edu>
2020-09-07 18:40:45 -0500

    Modal component

---
+++ web/components/Modal.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Modal.module.css";
+
+export const Modal: React.FC<{}> = ({children}) => (
+    <div className={ styles.modal }>
+        <div className={ styles.modalContent }>
+            { children }
+        </div>
+    </div>
+)
+Modal.displayName = "Modal";
