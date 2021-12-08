bdeae08119b330144d5fc713a3ba498459ea4a78
Carter Green <cartergr@usc.edu>
2021-12-07 22:27:15 -0600

    Rm web

--- web/index.ts
+++
@@ -1,8 +0,0 @@
-import { createElement } from "react";
-import { render } from "react-dom";
-import { App } from "./App";
-
-const GRID_COLOR = "#0f0f0f";
-
-const container = document.getElementById("app-container") as HTMLDivElement;
-render(createElement(App), container);
--- web/styles.d.ts
+++
@@ -1,5 +0,0 @@
-// For CSS
-declare module "*.module.css" {
-    const classes: { [key: string]: string };
-    export default classes;
-}
